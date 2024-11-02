6d4a278d23bd2d585165c93e7d2ed79fb1e801bf
Thomas Müller <mimmi20@live.de>
2024-11-02 13:32:52 +0100

    update sql

--- src/PdoSource.php
+++ src/PdoSource.php
@@ -71,7 +71,7 @@ final class PdoSource implements OutputAwareInterface, SourceInterface
      */
     public function getProperties(string $message, int &$messageLength = 0): iterable
     {
-        $sql = 'SELECT DISTINCT SQL_BIG_RESULT HIGH_PRIORITY `headers` FROM `request` ORDER BY `date` DESC, `count` DESC, `id` DESC';
+        $sql = 'SELECT DISTINCT SQL_BIG_RESULT HIGH_PRIORITY `headers`, `date`, `count`, `id` FROM `request` ORDER BY `date` DESC, `count` DESC, `id` DESC';
 
         $driverOptions = [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY];
 
