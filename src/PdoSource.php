8e431338f2e785775f15b80f76ec1e356e6cce53
Thomas Müller <mimmi20@live.de>
2025-08-13 20:20:02 +0200

    drop support for PHP 8.3

--- src/PdoSource.php
+++ src/PdoSource.php
@@ -74,7 +74,7 @@ final class PdoSource implements OutputAwareInterface, SourceInterface
      * @phpcsSuppress SlevomatCodingStandard.Functions.UnusedParameter.UnusedParameter
      */
     #[Override]
-    public function getProperties(string $message, int &$messageLength = 0): iterable
+    public function getProperties(string $parentMessage, int &$messageLength = 0): iterable
     {
         $sql = 'SELECT DISTINCT SQL_BIG_RESULT HIGH_PRIORITY `headers`, `date`, `count`, `id` FROM `request` ORDER BY `date` DESC, `count` DESC, `id` DESC';
 
